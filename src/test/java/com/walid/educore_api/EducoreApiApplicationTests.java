76adc1957ccee403d8745cab8af8b28b423fd8b9
waliiid3 <binwalid670@gmail.com>
2026-07-10 21:04:06 +0600

    Initial local Spring Boot project

---
+++ src/test/java/com/walid/educore_api/EducoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walid.educore_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
